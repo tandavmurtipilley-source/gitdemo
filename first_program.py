b1369f6e743dbf5472188742fefa30f769ebb591
tandavmurtipilley-source <tandavmurti.pilley@gmail.com>
2025-11-15 17:41:02 +0530

    Update first_program.py

--- first_program.py
+++ first_program.py
@@ -4,4 +4,7 @@ b = int(input("Enter number 2: "))
 print("The substratctin is ",(a-b))
 print("The addition is ",(a+b))
 print("The multiplication is ",(a*b))
+
 print("The division is ",a/b)
+
+##Demo
